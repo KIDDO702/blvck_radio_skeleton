7e4337f16d8f6fded94d46c04c6cc61b06aaa0cd
Robert Ochieng <robarangs57@gmail.com>
2023-08-15 16:53:11 +0300

    Navigation-1

---
+++ config/tailwind.config.js
@@ -0,0 +1,32 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                yellow: {
+                    primary: '#EAAE1C',
+                    light: '#F6DEA4'
+                },
+
+                dark: {
+                    primary: '#1C2326',
+                    secondary: '#21282B'
+                },
+
+                red: {
+                    primary: '#8B0101',
+                    secondary: '#96LALA',
+                    tertiary: '#A23333',
+                    light: '#D09999',
+                }
+            },
+
+            container: {
+                center: true
+            },
+
+            fontFamily: {
+                bricolageGrotesque: ['Bricolage Grotesque', 'sans-serif']
+            }
+        }
+    }
+}
